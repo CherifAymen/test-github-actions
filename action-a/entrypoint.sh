aa0cd78a58112ba01d93bbee21f4b4f5f3ee794a
CherifAymen <aymen.cherif@supcom.tn>
2020-08-17 17:19:15 +0200

    final test

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -2,6 +2,6 @@
 
 for i in `seq 1 100`;
 do
-      curl -s http://51.105.156.32:8080/hello
+      curl -s http://51.105.158.240:8080/hello
       sleep 1
 done 
--- src/main/java/com/sa/web/SentimentController.java
+++ src/main/java/com/sa/web/SentimentController.java
@@ -12,7 +12,7 @@ public class SentimentController {
 
     @GetMapping("/hello")
     public String  sayHello() {
-    	return "Bonjour Aymen !" ;
+    	return "Bonjour Aymen From JAVA !" ;
     }
 
     @GetMapping("/testHealth")
