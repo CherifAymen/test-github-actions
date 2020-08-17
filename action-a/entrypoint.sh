69c094d3aba3b12fa7233b962e8b48759f06e7ee
CherifAymen <42348641+CherifAymen@users.noreply.github.com>
2020-08-17 15:05:14 +0200

    Update entrypoint.sh

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -1,7 +1,8 @@
-#!/bin/bash 
- COUNTER=0
- while [  $COUNTER -lt 300 ]; do
-     curl http://51.105.156.32:8080/hello
-     sleep 1
-     let COUNTER=COUNTER+1 
- done
+#!/bin/sh -l
+
+sh -c "COUNTER=0 \
+ while [  $COUNTER -lt 300 ]; do \
+     curl http://51.105.156.32:8080/hello \
+     sleep 1 \
+     let COUNTER=COUNTER+1 \
+ done"
